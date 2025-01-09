2bca6ad99f66dc43c6e5cdbca3450b9062086a19
Seokho Son <shsongist@gmail.com>
2025-01-09 21:24:31 +0900

    Map customization

--- meetmap.py
+++ meetmap.py
@@ -44,13 +44,10 @@ def load_alias_data():
             return json.load(file)
     except FileNotFoundError:
         print("alias.json file not found.")
-        sys.exit(1)
     except json.JSONDecodeError:
         print("Error decoding alias.json file.")
-        sys.exit(1)
     except Exception as e:
         print(f"An unexpected error occurred while loading alias.json: {e}")
-        sys.exit(1)
 
 alias_data = load_alias_data()
 
@@ -243,12 +240,12 @@ def load_results_from_json(json_file='map.json', customization_file='map-customi
                 for key, value in customization_data.items():
                     if key in data:
                         data[key].update(value)
-                        print(f"Customization data for {key}{value} has been merged successfully.")
-                        print(f"Current data for {key}: {data[key]}")
+                        # print(f"Customization data for {key}{value} has been merged")
+                        # print(f"Current data for {key}: {data[key]}")
                     else:
                         data[key] = value
-                        print(f"Customization data for {key}{value} has been added successfully.")
-                        print(f"Current data for {key}: {data[key]}")
+                        # print(f"Customization data for {key}{value} has been added")
+                        # print(f"Current data for {key}: {data[key]}")
                 # print("Customization data has been merged successfully.")
                 # print("Merged data:")
                 # print(json.dumps(data, indent=4, ensure_ascii=False))
@@ -430,15 +427,45 @@ def list_apis():
         })
     return jsonify(routes)
 
+def sort_key(name):
+    """
+    Sort key function to sort room numbers based on the pattern '2-2', '10-1', etc.
+    - Sorts numerically before and after the '-' character.
+    """
+    parts = name.split('-')
+    
+    # Convert the first part to an integer for numeric sorting
+    first_part = int(parts[0])
+    
+    # Check if the second part is a number
+    second_part = parts[1]
+    if second_part.isdigit():
+        second_part = (0, int(second_part))  # Numbers have higher priority (0)
+    else:
+        second_part = (1, second_part)  # Strings have lower priority (1)
+
+    return (first_part, second_part)
+
 @app.route('/room', methods=['GET'])
 def list_room_numbers():
     """
-    list all available room numbers.
+    List all available room numbers.
     :return: JSON response with a list of all room numbers.
     """
     analyzed_results = load_results_from_json()
-    sorted_room_numbers = sorted(analyzed_results.keys())
-    return jsonify(sorted_room_numbers)
+    sorted_room_numbers = sorted(analyzed_results.keys(), key=sort_key)
+    print(f"Total Room Numbers: {len(sorted_room_numbers)}")
+    print(sorted_room_numbers)
+    response = {
+        "Total room numbers": len(sorted_room_numbers),
+        "All identified Rooms": sorted_room_numbers
+    }
+    return Response(
+        json.dumps(response, ensure_ascii=False),  # Ensure UTF-8 encoding
+        content_type="application/json; charset=utf-8",
+        status=200
+    )
+
 
 @app.route('/alias', methods=['GET'])
 def get_alias():
@@ -503,23 +530,37 @@ def is_number_in_range(num_str, range_str):
     return num_str in expanded_range
 
 @app.route('/room/<room_number>', methods=['GET'])
+@app.route('/view/<room_number>', methods=['GET'])
+@app.route('/find/<room_number>', methods=['GET'])
+@app.route('/dir/<room_number>', methods=['GET'])
+@app.route('/r/<room_number>', methods=['GET'])
+@app.route('/v/<room_number>', methods=['GET'])
+@app.route('/f/<room_number>', methods=['GET'])
+@app.route('/d/<room_number>', methods=['GET'])
 def view_room_highlighted(room_number):
     """
-    view an image with a specific room number highlighted.
+    View an image with a specific room number highlighted.
     This endpoint sends an image with the specified room number highlighted, indicating its location.
     :param room_number: The room number to be highlighted in the image.
-    :return: Image response with the specified room number highlighted, or an error message if not found.
+    :return: HTML response with the image or file response based on returnType parameter.
     """
 
     try:
+        # Replace multiple consecutive "-" with a single "-"
+        room_number = re.sub(r'-+', '-', room_number)
+
         # Normalize room_number by removing spaces
         normalized_room_number = room_number.replace(" ", "")
         
+        global alias_data
+        alias_data = load_alias_data()
+
         # Check alias_data for a matching key
         for alias, replacement in alias_data.items():
             normalized_alias = alias.replace(" ", "")
+            # print(f"Normalized Alias: {normalized_alias}")
             if normalized_alias in normalized_room_number:
-                room_number = re.sub(re.escape(alias), replacement, room_number, flags=re.IGNORECASE)
+                room_number = re.sub(re.escape(normalized_alias), replacement, normalized_room_number, flags=re.IGNORECASE)
                 break    
     
         # Define the patterns to be replaced
@@ -531,6 +572,10 @@ def view_room_highlighted(room_number):
         
         # Remove "호" from the room number
         room_number = room_number.replace("호", "")
+
+        # Replace multiple consecutive "-" with a single "-"
+        room_number = re.sub(r'-+', '-', room_number)
+
         room_number = room_number.upper()
 
         print(f"Requested Room Number: {room_number}")
@@ -539,19 +584,132 @@ def view_room_highlighted(room_number):
 
         # Check if room_number starts with any of the image names or if it exists in analyzed_results
         if room_number not in analyzed_results and not any(room_number.startswith(name.upper()) for name in image_names):
-            supported_maps = ", ".join(sorted(image_names))
-            return jsonify({"Message": "Map for " + room_number + " is not supported yet.", "Supported Maps": supported_maps}), 404
-        
+            response = {
+                "Message": f"Map for {room_number} is not supported yet.",
+                "Supported Maps": sorted(image_names, key=sort_key)
+            }
+            return Response(
+                json.dumps(response, ensure_ascii=False),  # Ensure non-ASCII characters are preserved
+                content_type="application/json; charset=utf-8",
+                status=404
+            )
         force = request.args.get('force', '').lower() == 'true'
         highlighted_image_path = view_room_highlighted_logic(room_number, force)
 
-        if os.path.exists(highlighted_image_path):
+        if not os.path.exists(highlighted_image_path):
+            return jsonify({"error": "Room number not found or image could not be created"}), 404
+
+        return_type = request.args.get('returnType', '').lower()
+
+        if return_type == "file":
+            # Return image file directly
             return send_file(highlighted_image_path, mimetype='image/png')
         else:
-            return jsonify({"error": "Room number not found or image could not be created"}), 404
+            # Return HTML with image embedded and styled
+            html_template = f"""
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Find Room: {room_number}</title>
+                <style>
+                    body {{
+                        margin: 0;
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        height: 100vh;
+                        background-color: #f0f0f0;
+                        position: relative;
+                        flex-direction: column;
+                    }}
+                    img {{
+                        max-width: 100%;
+                        max-height: 100%;
+                        object-fit: contain;
+                        transition: all 0.3s ease-in-out;
+                    }}
+                    .room-number {{
+                        position: absolute;
+                        bottom: 10px;
+                        left: 10px;
+                        color: white;
+                        background-color: rgba(0, 0, 0, 0.7);
+                        padding: 5px 10px;
+                        border-radius: 5px;
+                        font-family: Arial, sans-serif;
+                        font-size: 14px;
+                    }}
+                    .button-container {{
+                        position: absolute;
+                        bottom: 10px;
+                        right: 10px;
+                    }}
+                    button {{
+                        padding: 10px 15px;
+                        margin: 5px;
+                        font-size: 14px;
+                        border: none;
+                        border-radius: 5px;
+                        cursor: pointer;
+                        background-color: #007BFF;
+                        color: white;
+                    }}
+                    button:hover {{
+                        background-color: #0056b3;
+                    }}
+                </style>
+            </head>
+            <body>
+                <div class="button-container">
+                    <button id="toggleSizeButton" onclick="toggleImageSize()">+</button>
+                </div>
+                <img id="highlightedImage" src="data:image/png;base64,{convert_image_to_base64(highlighted_image_path)}" alt="Highlighted Room: {room_number}">
+                <div class="room-number">Requested Room: {room_number}</div>
+                <script>
+                    let isOriginalSize = false;
+
+                    function toggleImageSize() {{
+                        const img = document.getElementById('highlightedImage');
+                        const button = document.getElementById('toggleSizeButton');
+
+                        if (isOriginalSize) {{
+                            img.style.maxWidth = "100%";
+                            img.style.maxHeight = "100%";
+                            img.style.width = "auto";
+                            img.style.height = "auto";
+                            button.textContent = "+";
+                        }} else {{
+                            img.style.maxWidth = "none";
+                            img.style.maxHeight = "none";
+                            img.style.width = "auto";
+                            img.style.height = "auto";
+                            button.textContent = "fit";
+                        }}
+
+                        isOriginalSize = !isOriginalSize;
+                    }}
+                </script>
+            </body>
+            </html>
+            """
+            return html_template, 200, {'Content-Type': 'text/html'}
 
     except Exception as e:
         return jsonify({"Message": f"An error occurred: {e}"}), 500
+
+def convert_image_to_base64(image_path):
+    """
+    Convert an image file to a Base64 string for embedding in HTML.
+    :param image_path: Path to the image file.
+    :return: Base64 encoded string.
+    """
+    import base64
+    with open(image_path, "rb") as img_file:
+        return base64.b64encode(img_file.read()).decode('utf-8')
+
+
 
 def view_room_highlighted_logic(room_number, force=False):
     """
@@ -573,14 +731,12 @@ def view_room_highlighted_logic(room_number, force=False):
     if room_number in analyzed_results:
         room_info = analyzed_results[room_number]
         similar_room = None  # Exact match found
-        print(f"Exact match found for {room_number}")
     else:
         similar_room = calculate_room_similarity(room_number, analyzed_results.keys())
         if not similar_room or similar_room == room_number:  # Low similarity or no match found
             return None
         room_info = analyzed_results[similar_room]
-        print(f"Similar room found for {room_number}")
-              
+
     # Load the floor image and prepare for drawing
     floor_image_path = os.path.join(tmp_directory, f"{room_info['floor']}-map.png")
     if os.path.exists(floor_image_path):
@@ -601,7 +757,6 @@ def view_room_highlighted_logic(room_number, force=False):
         draw.rectangle(((x, y), (x + w, y + h)), outline="yellow", width=7)
         draw.rectangle(((x, y), (x + w, y + h)), outline="red", width=3)
 
-
         # Draw arrow and text
         path = calculate_arrow_path(image_center_x, image_center_y, x, y, w, h)
         for i in range(len(path) - 1):
@@ -615,33 +770,67 @@ def view_room_highlighted_logic(room_number, force=False):
         mark_pos = (path[-1][0], path[-1][1])
         draw_label(draw, message1 , mark_pos)
 
-        # Check for additional image in image/room directory
+        # Extract building number from room_number
+        building_number = room_number.split('-')[0]
+
+        # Check for additional images
         room_image_path = os.path.join("image/room", f"{room_number}.jpg")
+        skyview_image_path = os.path.join("image/skyview", f"{building_number}.jpg")
+
+        room_image = None
+        skyview_image = None
+
         if os.path.exists(room_image_path):
             room_image = Image.open(room_image_path)
-            
-            # Resize the room image based on the height of the main image
-            new_height = image.size[1] // 3
-            aspect_ratio = room_image.size[0] / room_image.size[1]
+
+        if os.path.exists(skyview_image_path):
+            skyview_image = Image.open(skyview_image_path)
+            new_height = (2 * image.size[1]) // 3  # Adjust skyview_image to 2/3 height of the main image
+            aspect_ratio = skyview_image.size[0] / skyview_image.size[1]
             new_width = int(new_height * aspect_ratio)
+            skyview_image = skyview_image.resize((new_width, new_height))
+
+        if room_image:
+            # Adjust room_image to match skyview_image width
+            new_width = skyview_image.size[0] if skyview_image else image.size[0] // 3
+            aspect_ratio = room_image.size[1] / room_image.size[0]
+            new_height = int(new_width * aspect_ratio)
             room_image = room_image.resize((new_width, new_height))
 
-            # Create a new image with enough width to accommodate both images
-            combined_width = image.size[0] + room_image.size[0]
-            combined_image = Image.new('RGB', (combined_width, image.size[1]))
+        # Combine images
+        combined_width = image.size[0] + (skyview_image.size[0] if skyview_image else 0)
+        combined_image_height = max(image.size[1], (room_image.size[1] if room_image else 0) + (skyview_image.size[1] if skyview_image else 0))
+        combined_image = Image.new('RGB', (combined_width, combined_image_height))
 
-            # Paste the main image and the room image side by side
-            combined_image.paste(image, (0, 0))
-            combined_image.paste(room_image, (image.size[0], (image.size[1] - new_height) // 2))
+        # Paste main image
+        combined_image.paste(image, (0, 0))
 
-            # Save the combined image
-            combined_image.save(highlighted_image_path)
-        else:
-            # Save the main image if no additional image is found
-            image.save(highlighted_image_path)
+        # Paste skyview image at the top right
+        if skyview_image:
+            if room_image:
+                combined_image.paste(skyview_image, (image.size[0], 0))
+            else:
+                # Calculate y offset to center skyview_image vertically relative to image
+                y_offset = (image.size[1] - skyview_image.size[1]) // 2
+                # Paste skyview image at the right of the main image, centered vertically
+                combined_image.paste(skyview_image, (image.size[0], y_offset))
+
+        # Paste room image below skyview image and center vertically
+        if room_image:
+            y_offset = skyview_image.size[1] if skyview_image else 0
+            remaining_space = combined_image_height - y_offset - room_image.size[1]
+            centered_y = y_offset + (remaining_space // 2)
+            combined_image.paste(room_image, (image.size[0], centered_y))
+
+        # Save the combined image
+        combined_image.save(highlighted_image_path)
+
         return highlighted_image_path
     else:
         return None
+
+
+
 
 def combine_images(tmp_directory, image_pattern):
     """
@@ -681,23 +870,23 @@ def combine_images(tmp_directory, image_pattern):
 def validate_images():
     """
     combine multiple images into one and return the combined image.
-    If a 'testset' query parameter is provided, it reads room numbers from 'testset.txt',
+    If a 'test' query parameter is provided, it reads room numbers from 'test.txt',
     generates images for each room number using view_room_highlighted, and combines these images.
     The 'force' query parameter can be used to force the regeneration of the combined image.
 
-    - 'testset=true': Generates and combines images for room numbers read from 'testset.txt'.
+    - 'test=true': Generates and combines images for room numbers read from 'test.txt'.
     - 'force=true': Ignores any cached combined image and forces the creation and combination of new images.
     -  Without 'force' parameter or with 'force=false': If a previously generated combined image exists, it's returned.   
 
     :return: Response with the combined image file or an error message.
     """
-    testset = request.args.get('testset')
+    test = request.args.get('test')
     force = request.args.get('force')
 
 
-    # If the testset parameter is provided, perform validation on the testset
-    if testset:
-        combined_image_path = os.path.join(tmp_directory, 'combined_testset_image.png')
+    # If the test parameter is provided, perform validation on the test
+    if test:
+        combined_image_path = os.path.join(tmp_directory, 'combined_test_image.png')
 
         if not force:
             if os.path.exists(combined_image_path):
@@ -732,7 +921,7 @@ def validate_images():
             combined_image.save(combined_image_path)
             return send_file(combined_image_path, mimetype='image/png')
         else:
-            return jsonify({"error": "No images were generated from the testset"}), 404
+            return jsonify({"error": "No images were generated from the test"}), 404
     else:
         combined_image_path = os.path.join(tmp_directory, 'combined_image.png')
 
@@ -783,7 +972,7 @@ if __name__ == '__main__':
             # If the user chooses not to perform new analysis, use the existing map.json file
             print("Using existing map.json file to run the server.")
             with open(json_file, 'r') as f:
-                analyzed_results = json.load(f)  # Load the analyzed results from the existing file
+                analyzed_results = load_results_from_json  # Load the analyzed results from the existing file
         else:
             # If the user chooses to perform new analysis
             analyzed_results = perform_image_analysis()  # Perform image analysis to extract room data
